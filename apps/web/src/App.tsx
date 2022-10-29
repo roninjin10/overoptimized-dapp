7617bdc836ccb9949e8247198d50400b55a5f6ab
Will Cory <willcory@Wills-MacBook-Pro.local>
2022-10-29 13:16:17 -0700

    install things

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,32 +1,12 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useReducer } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const [count, increment] = useReducer((c) => c + 1, 0);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount(count => count + 1)}>count is: {count}</button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header>App</header>
+      <button onClick={increment}>{count}</button>
     </div>
-  )
-}
-
-export default App
+  );
+};
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import "@rainbow-me/rainbowkit/styles.css";
 
-ReactDOM.render(
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import { App } from "./App";
+
+createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  </React.StrictMode>
+);
